16e63d3813d8eaa7d0397393f52f48af16f9f52d
thirunavuarasu <thirunavuarasu.m@gmail.com>
2024-08-10 14:31:28 +0530

    2

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h1>404 - Not Found</h1>
+      <h1>404 - Not Found,</h1>
       <p>The page you are looking for does not exist.</p>
     </div>
   );
